6d08f7d11925c3b427f3e9ba19cbf75eef8e73ce
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-13 19:49:21 +0100

    LambdaCompilation.java + tests

--- src/main/java/LambdaCompilation.java
+++ src/main/java/LambdaCompilation.java
@@ -3,10 +3,23 @@ import java.util.function.Supplier;
 /**
  * Created by mtumilowicz on 2018-11-13.
  */
-public class LambdaCompilation {
-    public String return_this() {
+class LambdaCompilation {
+    
+    String return_this() {
         Supplier<String> supplier = this::toString;
 
         return supplier.get();
+    }
+
+    String return_lambda_class() {
+        Supplier<String> supplier = this::toString;
+
+        return supplier.getClass().toString();
+    }
+    
+    void lambda_exception() {
+        Supplier<String> supplier = () -> {throw new RuntimeException();};
+        
+        supplier.get();
     }
 }
--- src/test/java/LambdaCompilationTest.java
+++ src/test/java/LambdaCompilationTest.java
@@ -1,7 +1,5 @@
 import org.junit.Test;
 
-import java.util.function.Supplier;
-
 import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -9,29 +7,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
  * Created by mtumilowicz on 2018-11-13.
  */
 public class LambdaCompilationTest {
-    
+
     @Test
     public void thisTest() {
-        Supplier<String> supplier = this::toString;
-        
-        assertThat(supplier.get(), containsString("LambdaCompilationTest"));
+        LambdaCompilation lambdaCompilation = new LambdaCompilation();
+
+        assertThat(lambdaCompilation.return_this(), containsString("LambdaCompilation"));
     }
 
     @Test
     public void lambdaClass() {
-        Supplier<String> supplier = () -> "empty";
+        LambdaCompilation lambdaCompilation = new LambdaCompilation();
 
-        assertThat(supplier.getClass().toString(), containsString("LambdaCompilationTest$$Lambda$40"));
+        assertThat(lambdaCompilation.return_lambda_class(), containsString("LambdaCompilation$$Lambda$40"));
     }
 
     @Test
     public void exception() {
-        Supplier<String> supplier = () -> {throw new RuntimeException();};
-
+        LambdaCompilation lambdaCompilation = new LambdaCompilation();
+        
         try {
-            supplier.get();
+            lambdaCompilation.lambda_exception();
         } catch (RuntimeException ex) {
-            System.out.println(ex.getStackTrace()[0]);
+            assertThat(ex.getStackTrace()[0].toString(), containsString("LambdaCompilation.lambda$lambda_exception$0"));
+            assertThat(ex.getStackTrace()[1].toString(), containsString("LambdaCompilation.lambda_exception"));
+            assertThat(ex.getStackTrace()[2].toString(), containsString("LambdaCompilationTest.exception"));
+            ex.printStackTrace();
         }
     }
 }
